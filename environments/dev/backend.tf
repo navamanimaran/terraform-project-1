24f06163cd28513862bbd62029685f3e1c6b6cc7
navamani <navamani@example.com>
2026-02-10 22:51:25 +0530

    s3 backend

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket         = "ci-cd-infra-pipeline-state-bucket-org-123"
-    region         = "us-west-1"
-    key            = "dev/terraform.tfstate"
-    dynamodb_table = "terraform_state_locks"
-    encrypt        = true
-  }
-}
+bucket         = "ci-cd-infra-pipeline-state-bucket-org-123"
+region         = "us-west-1"
+key            = "dev/terraform.tfstate"
+dynamodb_table = "terraform_state_locks"
+encrypt        = true
