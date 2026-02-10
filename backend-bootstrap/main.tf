d6285432fa2da092207ffb9a1ee1fb86eaf76cfa
navamani <navamani@example.com>
2026-02-10 22:41:47 +0530

    terraform fmt

--- backend-bootstrap/main.tf
+++ backend-bootstrap/main.tf
@@ -1,50 +1,50 @@
 provider "aws" {
-    region = "us-west-1"  
+  region = "us-west-1"
 }
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "ci-cd-infra-pipeline-state-bucket-org-123"
-    lifecycle {
-      prevent_destroy = true
-    }
+  bucket = "ci-cd-infra-pipeline-state-bucket-org-123"
+  lifecycle {
+    prevent_destroy = true
+  }
 
-    tags = {
-        Name        = "Terraform State Bucket"
-        project     = "CI-CD Infra Pipeline"
-    }
+  tags = {
+    Name    = "Terraform State Bucket"
+    project = "CI-CD Infra Pipeline"
+  }
 }
 resource "aws_s3_bucket_versioning" "Enabled" {
-    bucket = aws_s3_bucket.terraform_state.id
-    versioning_configuration {
-        status = "Enabled"
-    }
-  
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-    bucket = aws_s3_bucket.terraform_state.id
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 resource "aws_s3_bucket_public_access_block" "public_access" {
-    bucket = aws_s3_bucket.terraform_state.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform_state_locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-    tags = {
-        Name        = "Terraform Locks Table"
-        project     = "CI-CD Infra Pipeline"
-    }
-  
+  name         = "terraform_state_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    Name    = "Terraform Locks Table"
+    project = "CI-CD Infra Pipeline"
+  }
+
 }
